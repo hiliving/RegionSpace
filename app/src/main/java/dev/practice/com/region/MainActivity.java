9b40f27dd34c038d8534ac5fcb6198e28c46880c
hiliving <1195996300@qq.com>
2017-06-28 14:43:55 +0800

    增加了一个实例

--- app/src/main/java/dev/practice/com/region/MainActivity.java
+++ app/src/main/java/dev/practice/com/region/MainActivity.java
@@ -2,15 +2,46 @@ package dev.practice.com.region;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RegionRemoteControl control;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        control = (RegionRemoteControl) findViewById(R.id.control);
+        control.setLisener(new RegionRemoteControl.MenuListener() {
+            @Override
+            public void onCenterClick() {
+                Toast.makeText(MainActivity.this,"中间点击了",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUpClick() {
+                Toast.makeText(MainActivity.this,"上点击了",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightClick() {
+                Toast.makeText(MainActivity.this,"右点击了",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownClick() {
+                Toast.makeText(MainActivity.this,"下点击了",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLeftClick() {
+                Toast.makeText(MainActivity.this,"左点击了",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/dev/practice/com/region/RegionRemoteControl.java
+++ app/src/main/java/dev/practice/com/region/RegionRemoteControl.java
@@ -1,6 +1,7 @@
 package dev.practice.com.region;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
@@ -9,6 +10,7 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Region;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -18,7 +20,7 @@ import android.view.View;
 public class RegionRemoteControl extends View {
     Path up_p,down_p,left_p,right_p,center_p;
     Region up,down,left,right,center;
-    Matrix mMatrix = null;
+    Matrix mMapMatrix = null;
     int CENTER = 0;
     int UP = 1;
     int RIGHT = 2;
@@ -27,6 +29,7 @@ public class RegionRemoteControl extends View {
     int touchFlag = -1;
     int currentFlag = -1;
     int mDefaultColor = Color.GRAY;
+    int mTouchedColor = Color.DKGRAY;
     private Paint mDefaultPaint;
     public RegionRemoteControl(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -40,24 +43,150 @@ public class RegionRemoteControl extends View {
         down = new Region();
         left = new Region();
         right = new Region();
+        center = new Region();
 
         mDefaultPaint = new Paint();
         mDefaultPaint.setColor(mDefaultColor);
         mDefaultPaint.setAntiAlias(true);
 
-        mMatrix = new Matrix();
+        mMapMatrix = new Matrix();
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mMatrix.reset();
+        mMapMatrix.reset();
         //注意这个区域的大小
         Region globalRegion = new Region(-w,-h,w,h);
         int minWidth = w>h?h:w;
         minWidth*=0.8;
         int br = minWidth /2;
         RectF bigCircle = new RectF(-br,-br,br,br);
+
+        int sr = minWidth / 4;
+        RectF smallCircle = new RectF(-sr,-sr,sr,sr);
+
+        float bigSweepAngle = 84;
+        float smallSweepAngle = -80;
+
+        //根据视图大小，初始化Path 和 Region
+        center_p.addCircle(0,0,0.2f*minWidth, Path.Direction.CCW);
+        center.setPath(center_p,globalRegion);
+
+        right_p.addArc(bigCircle,-40,bigSweepAngle);
+        right_p.arcTo(smallCircle,40,smallSweepAngle);
+        right_p.close();
+        right.setPath(right_p,globalRegion);
+
+        down_p.addArc(bigCircle,50,bigSweepAngle);
+        down_p.arcTo(smallCircle,130,smallSweepAngle);
+        down_p.close();
+        down.setPath(down_p,globalRegion);
+
+        left_p.addArc(bigCircle,140,bigSweepAngle);
+        left_p.arcTo(smallCircle,220,smallSweepAngle);
+        left_p.close();
+        left.setPath(left_p,globalRegion);
+
+        up_p.addArc(bigCircle,230,bigSweepAngle);
+        up_p.arcTo(smallCircle,310,smallSweepAngle);
+        up_p.close();
+        up.setPath(up_p,globalRegion);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float[] pts = new float[2];
+        pts[0] = event.getRawX();
+        pts[1] = event.getRawY();
+        mMapMatrix.mapPoints(pts);
+
+        int x = (int) pts[0];
+        int y = (int) pts[1];
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                touchFlag = getTouchedPath(x,y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                currentFlag = getTouchedPath(x,y);
+                break;
+            case MotionEvent.ACTION_UP:
+                currentFlag = getTouchedPath(x,y);
+                //如果手指按下区域和抬起区域相同且不为空，则判断点击事件
+                if (currentFlag==touchFlag&&currentFlag!=-1&&listener!=null){
+                    if (currentFlag==CENTER){
+                        listener.onCenterClick();
+                    }else if (currentFlag==RIGHT){
+                        listener.onRightClick();
+                    }else if (currentFlag==UP){
+                        listener.onUpClick();
+                    }else if (currentFlag==DOWN){
+                        listener.onDownClick();
+                    }else if (currentFlag==LEFT){
+                        listener.onLeftClick();
+                    }
+                }
+                touchFlag = currentFlag = -1;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                touchFlag = currentFlag = -1;
+                break;
+        }
+        invalidate();
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.translate(getWidth()/2,getHeight()/2);
+        //获取测量的逆矩阵
+        if (mMapMatrix.isIdentity()){
+            canvas.getMatrix().invert(mMapMatrix);
+        }
+        //绘制默认颜色
+        canvas.drawPath(center_p,mDefaultPaint);
+        canvas.drawPath(up_p,mDefaultPaint);
+        canvas.drawPath(right_p,mDefaultPaint);
+        canvas.drawPath(down_p,mDefaultPaint);
+        canvas.drawPath(left_p,mDefaultPaint);
+
+        //绘制触摸区域颜色
+        mDefaultPaint.setColor(mTouchedColor);
+        if (touchFlag==CENTER){
+            canvas.drawPath(center_p,mDefaultPaint);
+        }else if (touchFlag==RIGHT){
+            canvas.drawPath(right_p,mDefaultPaint);
+        }else if (touchFlag==UP){
+            canvas.drawPath(up_p,mDefaultPaint);
+        }else if (touchFlag==DOWN){
+            canvas.drawPath(down_p,mDefaultPaint);
+        }else if (touchFlag==LEFT){
+            canvas.drawPath(left_p,mDefaultPaint);
+        }
+        mDefaultPaint.setColor(mDefaultColor);
+    }
+
+    /**
+     * 获取当前触摸点在哪个区域
+     * @param x
+     * @param y
+     * @return
+     */
+    private int getTouchedPath(int x, int y) {
+        if (center.contains(x,y)){
+            return 0;
+        }else if (up.contains(x,y)){
+            return 1;
+        }else if (right.contains(x,y)){
+            return 2;
+        }else if (down.contains(x,y)){
+            return 3;
+        }else if (left.contains(x,y)){
+            return 4;
+        }
+        return -1;
     }
 
     public RegionRemoteControl(Context context, AttributeSet attrs, int defStyleAttr) {
